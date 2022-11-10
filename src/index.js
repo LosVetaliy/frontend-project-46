520270d821708aa614df392a0fb6c077f51098de
LosVetaliy <losvetaliy@gmail.com>
2022-11-10 12:10:25 +0300

    step3 path

--- src/index.js
+++ src/index.js
@@ -2,63 +2,7 @@ import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 //__fixtures__/file1.json
-const getDiffInfo = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  const uniq = _.uniq([...keys1, ...keys2]);
-  const sortUniq = _.sortBy(uniq);
-  const result = sortUniq.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    if ((value1 && value2) && (value1 !== value2)) {
-      return {
-        type: 'changed',
-        key,
-        value1,
-        value2,
-      };
-    }
-    if (!_.has(obj2, key)) {
-      return {
-        type: 'delited',
-        key,
-        value: value1,
-      };
-    }
-    if (!_.has(obj1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: value2,
-      };
-    }
-    return {
-      type: 'unchanged',
-      key,
-      value: value1,
-    };
-  });
-  return result;
-};
 
-const getDiff = (diffInfo) => {
-  const result = diffInfo.map((diff) => {
-    const typediff = diff.type;
-    switch (typediff) {
-      case 'delited':
-        return `  - ${diff.key}: ${diff.value}`;
-      case 'unchanged':
-        return `    ${diff.key}: ${diff.value}`;
-      case 'changed':
-        return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
-      case 'added':
-        return `  + ${diff.key}: ${diff.value}`;
-      default:
-        return null;
-    }
-  });
-  return `{\n${result.join('\n')}\n}`;
-};
 const getAbsolutPath = (filePath) => path.resolve(process.cwd(), filePath);
 const readFile = (filePath) => fs.readFileSync(getAbsolutPath(filePath), 'utf-8');
 const getObject = (filePath) => JSON.parse(readFile(filePath));
