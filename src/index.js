727f9a946ea3672451203c00b1853d429fbec612
LosVetaliy <losvetaliy@gmail.com>
2022-11-10 11:35:03 +0300

    step3 begin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import _ from "lodash"
+import _ from 'lodash';
+import path from 'path';
+import fs from 'fs'
 
-const genDiff = () => {
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
+const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const getObject = (filePath) => JSON.parse(readFile(filePath));
 
-}
-export default genDiff
+const genDiff = (filePath1, filePath2) => {
+    const object1 = getObject(filePath1);
+    const object2 = getObject(filePath2);
+    //return getDiff(getDiffInfo(object1, object2));
+    console.log (object1, object2)
+    return (object1, object2)
+};
+export default genDiff;
