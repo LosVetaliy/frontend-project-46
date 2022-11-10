af415a22d95df8702095871a1d04a5f17af391f3
LosVetaliy <losvetaliy@gmail.com>
2022-11-10 12:09:05 +0300

    step3 path fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,71 @@
 import _ from 'lodash';
 import path from 'path';
-import fs from 'fs'
+import fs from 'fs';
+//__fixtures__/file1.json
+const getDiffInfo = (obj1, obj2) => {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  const uniq = _.uniq([...keys1, ...keys2]);
+  const sortUniq = _.sortBy(uniq);
+  const result = sortUniq.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if ((value1 && value2) && (value1 !== value2)) {
+      return {
+        type: 'changed',
+        key,
+        value1,
+        value2,
+      };
+    }
+    if (!_.has(obj2, key)) {
+      return {
+        type: 'delited',
+        key,
+        value: value1,
+      };
+    }
+    if (!_.has(obj1, key)) {
+      return {
+        type: 'added',
+        key,
+        value: value2,
+      };
+    }
+    return {
+      type: 'unchanged',
+      key,
+      value: value1,
+    };
+  });
+  return result;
+};
 
-const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
-const readFile = (filepath) => fs.readFileSync(getAbsolutePath(filepath), 'utf-8');
+const getDiff = (diffInfo) => {
+  const result = diffInfo.map((diff) => {
+    const typediff = diff.type;
+    switch (typediff) {
+      case 'delited':
+        return `  - ${diff.key}: ${diff.value}`;
+      case 'unchanged':
+        return `    ${diff.key}: ${diff.value}`;
+      case 'changed':
+        return (`  - ${diff.key}: ${diff.value1} \n  + ${diff.key}: ${diff.value2}`);
+      case 'added':
+        return `  + ${diff.key}: ${diff.value}`;
+      default:
+        return null;
+    }
+  });
+  return `{\n${result.join('\n')}\n}`;
+};
+const getAbsolutPath = (filePath) => path.resolve(process.cwd(), filePath);
+const readFile = (filePath) => fs.readFileSync(getAbsolutPath(filePath), 'utf-8');
 const getObject = (filePath) => JSON.parse(readFile(filePath));
 
 const genDiff = (filePath1, filePath2) => {
-    const object1 = getObject(filePath1);
-    const object2 = getObject(filePath2);
-    //return getDiff(getDiffInfo(object1, object2));
-    console.log (object1, object2)
-    return (object1, object2)
+  const object1 = getObject(filePath1);
+  const object2 = getObject(filePath2);
+  return getDiff(getDiffInfo(object1, object2));
 };
 export default genDiff;
