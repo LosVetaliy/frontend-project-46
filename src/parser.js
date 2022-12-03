f7b91d1119ecffbf2d5913477352a396bea73b04
Dmitry Yakovlev <losvetaliy@gmail.com>
2022-12-03 22:26:24 +0300

    Add fixes to parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parse = (data, fileExtension) => {
-  switch (fileExtension) {
+const parse = (data, dataType) => {
+  switch (dataType) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
     case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`unknown file extension ${fileExtension}`);
+      throw new Error(`unknown file extension ${dataType}`);
   }
 };
 export default parse;
