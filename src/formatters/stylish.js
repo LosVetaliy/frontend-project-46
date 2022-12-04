e3c36fbedd15dd722793d0ce2bfb0fd85a9659f4
Dmitry Yakovlev <losvetaliy@gmail.com>
2022-12-05 01:17:26 +0300

    Stylish.js in progress

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,35 +1,33 @@
 import _ from 'lodash';
 
-const stylish = (diff) => {
+const stylish = (value, replacer = ' ', spacesCount = 1) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
       return `${currentValue}`;
     }
-    const replacer = ' ';
-    const spacesCount = 2;
     const indentSize = depth * spacesCount;
     const currentIndent = replacer.repeat(indentSize);
     const bracketIndent = replacer.repeat(indentSize - spacesCount);
-
-    const lines = _.sortBy(Object.entries(currentValue))
+    const lines = Object
+      .entries(currentValue)
       .map(([key, val]) => {
         if (val.difference === 'changed') {
-          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 2)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 2)}`;
+          return `${currentIndent}- ${key}: ${iter(val.value1, depth + 1)}\n${currentIndent}+ ${key}: ${iter(val.value2, depth + 1)}`;
         }
         if (val.difference === 'unchanged') {
           return `${currentIndent}  ${key}: ${val.value}`;
         }
         if (val.difference === 'nested') {
-          return `${currentIndent}  ${key}: ${iter(val.value, depth + 2)}`;
+          return `${currentIndent}  ${key}: ${iter(val.value, depth + 1)}`;
         }
         if (val.difference === 'added') {
-          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 2)}`;
+          return `${currentIndent}+ ${key}: ${iter(val.value, depth + 1)}`;
         }
         if (val.difference === 'deleted') {
-          return `${currentIndent}- ${key}: ${iter(val.value, depth + 2)}`;
+          return `${currentIndent}- ${key}: ${iter(val.value, depth + 1)}`;
         }
         if (_.isObject(val)) {
-          return `${currentIndent}  ${key}: ${iter(val, depth + 2)}`;
+          return `${currentIndent}  ${key}: ${iter(val, depth)}`;
         }
         return `${currentIndent}  ${key}: ${val}`;
       });
@@ -40,6 +38,7 @@ const stylish = (diff) => {
     ].join('\n');
   };
 
-  return iter(diff, 1);
+  return iter(value, 1);
 };
+
 export default stylish;
